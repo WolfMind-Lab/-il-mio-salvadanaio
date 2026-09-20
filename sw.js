9a5684cbb2b2707e68ecad07fc9423e84b34e409
WolfMind-Lab <wolfmind.it@gmail.com>
2026-09-20 23:24:59 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,37 @@
-const CACHE='salvadanaio-v1'; self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json'])))); self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='salvadanaio-v2';
+
+self.addEventListener('install',e=>{
+  e.waitUntil(
+    caches.open(CACHE).then(c=>c.addAll([
+      './',
+      './index.html',
+      './manifest.json'
+    ]))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate',e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>
+      Promise.all(
+        keys
+          .filter(k=>k!==CACHE)
+          .map(k=>caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch',e=>{
+  e.respondWith(
+    fetch(e.request)
+      .then(response=>{
+        const copy=response.clone();
+        caches.open(CACHE).then(c=>c.put(e.request,copy));
+        return response;
+      })
+      .catch(()=>caches.match(e.request))
+  );
+});
